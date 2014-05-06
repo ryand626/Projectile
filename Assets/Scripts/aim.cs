e2c3f2acda463bfd43abead3e410acf4878d24a4
Ryan Dougherty <ryand626@gmail.com>
2014-05-06 00:21:30 -0400

    restricted controls

--- Assets/Scripts/aim.cs
+++ Assets/Scripts/aim.cs
@@ -5,32 +5,38 @@ public class aim : MonoBehaviour {
 	public predict prediction;
 	public fire shoot;
 	void Update () {
-		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) {
-			transform.Rotate(0f,0f,1f);
-			prediction.initialPosition = transform.FindChild("launchpoint").transform.position;
-			prediction.initialVelocity = (gameObject.transform.FindChild("barrel").position - gameObject.transform.position) * shoot.launchVelocity;
-			prediction.UpdateTrajectory();
+		if (transform.rotation.eulerAngles.z < 359) {
+			if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) {
+				transform.Rotate (0f, 0f, 1f);
+				prediction.initialPosition = transform.FindChild ("launchpoint").transform.position;
+				prediction.initialVelocity = (gameObject.transform.FindChild ("barrel").position - gameObject.transform.position) * shoot.launchVelocity;
+				prediction.UpdateTrajectory ();
+			}
 		}
-		
-		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) {
-			transform.Rotate(0f,0f,-1f);
-			prediction.initialPosition = transform.FindChild("launchpoint").transform.position;
-			prediction.initialVelocity = (gameObject.transform.FindChild("barrel").position - gameObject.transform.position) * shoot.launchVelocity;
-			prediction.UpdateTrajectory();
+		if (transform.rotation.eulerAngles.z > 230) {
+			if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) {
+				transform.Rotate (0f, 0f, -1f);
+				prediction.initialPosition = transform.FindChild ("launchpoint").transform.position;
+				prediction.initialVelocity = (gameObject.transform.FindChild ("barrel").position - gameObject.transform.position) * shoot.launchVelocity;
+				prediction.UpdateTrajectory ();
+			}
 		}
 
+
 		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)) {
-			shoot.launchVelocity += .1f;
-			prediction.initialPosition = transform.FindChild("launchpoint").transform.position;
-			prediction.initialVelocity = (gameObject.transform.FindChild("barrel").position - gameObject.transform.position) * shoot.launchVelocity;
-			prediction.UpdateTrajectory();
+				shoot.launchVelocity += .1f;
+				prediction.initialPosition = transform.FindChild ("launchpoint").transform.position;
+				prediction.initialVelocity = (gameObject.transform.FindChild ("barrel").position - gameObject.transform.position) * shoot.launchVelocity;
+				prediction.UpdateTrajectory ();
 		}
-		if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S)) {
-			shoot.launchVelocity -= .1f;
-			prediction.initialPosition = transform.FindChild("launchpoint").transform.position;
-			prediction.initialVelocity = (gameObject.transform.FindChild("barrel").position - gameObject.transform.position) * shoot.launchVelocity;
-			prediction.UpdateTrajectory();
-		}	
+		if (shoot.launchVelocity >= 0) {
+			if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S)) {
+				shoot.launchVelocity -= .1f;
+				prediction.initialPosition = transform.FindChild ("launchpoint").transform.position;
+				prediction.initialVelocity = (gameObject.transform.FindChild ("barrel").position - gameObject.transform.position) * shoot.launchVelocity;
+				prediction.UpdateTrajectory ();
+			}	
+		}
 
 	}
 }
